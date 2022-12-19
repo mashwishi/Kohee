230c4d06d60bb7aa96d782651f994e3c2e5af233
Mashwishi <Mashwishiii@gmail.com>
2022-12-20 05:56:13 +0800

    1.1.0
    landing hot fix

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -86,7 +86,12 @@ const Landing: NextPage = () => {
                     </button>
 
                     <button onClick={() => { window.open('https://ko-fi.com/koheeApp');}} className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
-                        {`< Developer />`}
+                        <span className="mr-2">
+                            {`Support the Project`}   
+                        </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M.11 3.187A.5.5 0 0 1 .5 3h13a.5.5 0 0 1 .488.608l-.22.991a3.001 3.001 0 0 1-1.3 5.854l-.132.59A2.5 2.5 0 0 1 9.896 13H4.104a2.5 2.5 0 0 1-2.44-1.958L.012 3.608a.5.5 0 0 1 .098-.42Zm12.574 6.288a2 2 0 0 0 .866-3.899l-.866 3.9ZM1.124 4l1.516 6.825A1.5 1.5 0 0 0 4.104 12h5.792a1.5 1.5 0 0 0 1.464-1.175L12.877 4H1.123Z"/>
+                        </svg>
                     </button>
 
                 </div>
