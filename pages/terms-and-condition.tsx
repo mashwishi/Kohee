0d0d0917947d9ddfa6a4dca2d865cbac2520bad2
Mashwishi <Mashwishiii@gmail.com>
2022-12-31 17:27:55 +0800

    1.5.2
    card preview hotfix

--- pages/terms-and-condition.tsx
+++ pages/terms-and-condition.tsx
@@ -77,9 +77,7 @@ const TermsAndCondition = () => {
                         </li>
                       </ul>  
                       </p>
-                      <p className="text-gray-700 text-base">
-                      This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the Free Terms and Conditions Generator.
-                      </p>
+
                       <p className="text-gray-700 text-base">
                       Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. UNOVA TECHNOLOGY does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of UNOVA TECHNOLOGY,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, UNOVA TECHNOLOGY shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
                       </p>
