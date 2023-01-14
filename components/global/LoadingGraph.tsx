5c186b6be61c4c3e35fc45a7a88be2bd7645f71e
Mashwishi <Mashwishiii@gmail.com>
2023-01-15 04:10:43 +0800

    1.9.6
    Fixed no data api
    
    Co-Authored-By: Exaruss <95257350+Exaruss@users.noreply.github.com>

--- components/global/LoadingGraph.tsx
+++ components/global/LoadingGraph.tsx
@@ -2,7 +2,8 @@
 const LoadingGraph = () => {
     return (
         <>
-            <div className="flex items-center justify-center h-full px-4 py-24 text-gray-400 text-3xl font-semibold bg-gray-100 border-2 border-gray-200 border-dashed rounded-md">
+            {/* <div className="flex items-center justify-center h-full px-4 py-24 text-gray-400 text-3xl font-semibold bg-gray-100 border-2 border-gray-200 border-dashed rounded-md"> */}
+            <div className="flex items-center justify-center h-full px-4 py-24 text-gray-400 text-3xl font-semibold bg-gray-100">
                 <div className="animate-pulse flex space-x-4 w-12 py-4">
                     <div className="flex">
                         <div className="h-2 w-2 bg-slate-700 mr-1 rounded-full animate-pulse"></div>
