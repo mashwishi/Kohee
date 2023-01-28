5e8cfb055933bc7e6773ea577334cf42248ee9d4
Mashwishi <Mashwishiii@gmail.com>
2023-01-28 23:38:09 +0800

    2.1.3
    Co-Authored-By: Exaruss <95257350+Exaruss@users.noreply.github.com>

--- components/user/getUser_Preview.tsx
+++ components/user/getUser_Preview.tsx
@@ -409,18 +409,27 @@ const GetUser_Preview = (props: GetUser_Preview) => {
                             </div>    
                         </div>
 
-                        {/*Follow Function*/}
+                        {/* Start - Message, Follow and Dashboard Button */}
                         <div className="abosolute flex flex-row justify-end mr-4 px-2 mt-8">
-                            <div className="flex flex-row space-x-2">
+                            <div className="">
+                                    {/* Start Msg Button */}
+                                    <label htmlFor={`send-msg`} className="cursor-pointer rounded-full btn-primary btn-circle  btn-outline">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" className="bi bi-chat" viewBox="0 0 16 16">
+                                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                                        </svg>
+                                    </label>
+                                    {/* End Msg Button */}
                             </div>
-                            <div className="flex flex-row space-x-2">
-                                <div className="h-5 w-20">
-                                        <button className="rounded-xl px-3 py-1 font-semibold bg-secondary">
-                                        Follow
-                                        </button>
-                                </div>
+                            <div className="ml-2">
+                                        <span className="cursor-pointer rounded-full btn-primary btn-circle  btn-outline" >
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-add" viewBox="0 0 16 16">
+                                                <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
+                                                <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
+                                            </svg>
+                                        </span>
                             </div>
                         </div>
+                        {/* End - Message, Follow and Dashboard Button */}
 
                     </div>
 
